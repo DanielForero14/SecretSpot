d2db8125a5c2af7722bc3d1b883bdc892fb4a921
AnthonyNope <anthonynore@unisabana.edu.co>
2025-05-07 12:56:39 -0500

    Imagenes Home y ajustes visuales

--- app/home.tsx
+++ app/home.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
-        <Image source={require('../assets/images/icon.png')} style={styles.icon} />
+        <Image source={require('../assets/images/user-icon.png')} style={styles.icon} />
         <Text style={styles.headerText}>SecretSpot</Text>
-        <Image source={require('../assets/images/icon.png')} style={styles.icon} />
+        <Image source={require('../assets/images/menu-icon.png')} style={styles.icon} />
       </View>
 
-      {/* Question */}
-      <Text style={styles.question}>
-        What do you want to do <Text style={styles.highlight}>today</Text>?
-      </Text>
+      {/* Contenido central */}
+      <View style={styles.centerContent}>
+        <Text style={styles.question}>
+          What do you want to do <Text style={styles.highlight}>today</Text>?
+        </Text>
 
-      {/* Cards */}
-      <ScrollView contentContainerStyle={styles.cards}>
-        <TouchableOpacity style={styles.card}>
-          <Image source={require('../assets/images/explore.png')} style={styles.cardImage} />
-          <Text style={styles.cardText}>EXPLORE</Text>
-        </TouchableOpacity>
+        <View style={styles.cards}>
+          <TouchableOpacity style={styles.card}>
+            <Image source={require('../assets/images/explore.png')} style={styles.cardImage} />
+            <Text style={styles.cardText}>EXPLORE</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Image source={require('../assets/images/places.png')} style={styles.cardImage} />
-          <Text style={styles.cardText}>MY PLACES</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.card}>
+            <Image source={require('../assets/images/places.png')} style={styles.cardImage} />
+            <Text style={styles.cardText}>MY PLACES</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.card}>
-          <Image source={require('../assets/images/photos.png')} style={styles.cardImage} />
-          <Text style={styles.cardText}>MY PHOTOS</Text>
-        </TouchableOpacity>
-      </ScrollView>
-
-            {/* Bottom bar with logo */}
-        <View style={styles.footer}>
-            <View style={styles.logoWrapper}>
-                <Image source={require('../assets/images/Logo.png')} style={styles.bottomLogo} />
-            </View>
+          <TouchableOpacity style={styles.card}>
+            <Image source={require('../assets/images/photos.png')} style={styles.cardImage} />
+            <Text style={styles.cardText}>MY PHOTOS</Text>
+          </TouchableOpacity>
         </View>
+      </View>
 
+      {/* Bottom bar with logo */}
+      <View style={styles.footer}>
+        <View style={styles.logoWrapper}>
+          <Image source={require('../assets/images/Logo.png')} style={styles.bottomLogo} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -49,26 +49,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1c1c1c',
-    paddingTop: 50,
-    paddingHorizontal: 20,
-    justifyContent: 'space-between',
   },
   header: {
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 25,
-  },
-  icon: {
-    width: 28,
-    height: 28,
-    resizeMode: 'contain',
-    tintColor: '#fff',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
   },
   headerText: {
-    color: '#fff',
-    fontSize: 22,
+    color: '#1c1c1c',
+    fontSize: 20,
     fontWeight: 'bold',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+    tintColor: '#1c1c1c',
+  },
+  centerContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
   },
   question: {
     color: '#fff',
@@ -113,10 +121,9 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     position: 'relative',
   },
-  
   logoWrapper: {
     position: 'absolute',
-    top: -30, // para que se superponga
+    top: -30,
     backgroundColor: '#1c1c1c',
     padding: 15,
     borderRadius: 100,
@@ -125,10 +132,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
   bottomLogo: {
     width: 50,
     height: 50,
     resizeMode: 'contain',
-  },  
+  },
 });
